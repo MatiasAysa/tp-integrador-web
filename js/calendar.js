8b18ba839dedcfc5cb6416425c2fc685a1821f05
MatiasAysa <139184302+MatiasAysa@users.noreply.github.com>
2025-11-12 18:42:37 -0300

    Update calendar.js

--- js/calendar.js
+++ js/calendar.js
@@ -8,31 +8,25 @@ const hoy = new Date();
 let fechaActual = new Date();
 
 function renderCalendar(fecha) {
-    //limpia dias anteriores
     daysContainer.innerHTML = "";
 
     const anio = fecha.getFullYear();
     const mes = fecha.getMonth();
 
-    //actializa titulo
     monthElement.textContent = fecha.toLocaleString("es", { month: "long" });
     yearElement.textContent = anio;
 
-    //nro de dia(lunes = 1, martes= 2...)en el que empieza el mes
     const indicePrimerDia = new Date(anio, mes, 1).getDay();
 
-    //cantidad de dias del mes
     const ultimoDia = new Date(anio, mes + 1, 0).getDate();
 
-    //ajuste para q la semana empieze los lunes (lunes = 0, martes= 2..)
     let ajusteInicioDiaDeLaSemana;
-    if (indicePrimerDia === 0) {      //(domingo=6)
+    if (indicePrimerDia === 0) {    
         ajusteInicioDiaDeLaSemana = 6;
     } else {
         ajusteInicioDiaDeLaSemana = indicePrimerDia - 1;
     }
 
-    //crea dias vacios antes del 1
     let diasVacios = "";
     for (let i = 0; i < ajusteInicioDiaDeLaSemana; i++) {
         diasVacios += '<li class="calendar__day empty"></li>';
@@ -40,18 +34,15 @@ function renderCalendar(fecha) {
     daysContainer.innerHTML += diasVacios;
 
 
-    //crea los dias del mes
     let diasDelMes = "";
     for (let dia = 1; dia <= ultimoDia; dia++) {
 
         let isToday = dia == hoy.getDate() && mes == hoy.getMonth() && anio == hoy.getFullYear();
 
-        //obtiene eventos del dia
         let eventosDelDia = eventos.filter(function (ev) {
             return ev.dia === dia && ev.mes === mes + 1 && ev.anio === anio;
         });
 
-        //crea html de eventos
         let contenidoEventos = "";
         for (let j = 0; j < eventosDelDia.length; j++) {
             let evento= eventosDelDia[j];
@@ -61,33 +52,27 @@ function renderCalendar(fecha) {
             
         }
 
-        //agregar today y eventos
         diasDelMes += '<li class="calendar__day' + (isToday ? " today" : "") + '">';
         diasDelMes += '<h4>' + dia + '</h4>';
         diasDelMes += contenidoEventos;
         diasDelMes += '</li>';
     }
 
-    //lo agrega al container de dias
     daysContainer.innerHTML += diasDelMes;
 
-    //agrega eventos al hacer click
     agregarListenersEventos();
 }
 
 renderCalendar(fechaActual);
 
-//Botones para cambiar de mes
 const btnPrev = document.querySelector(".js-prev-month");
 const btnNext = document.querySelector(".js-next-month");
 
-//fncion mes anterior
 function mesAnterior() {
     fechaActual.setMonth(fechaActual.getMonth() - 1);
     renderCalendar(fechaActual);
 }
 
-//funcion mes siguiente
 function mesSiguiente() {
     fechaActual.setMonth(fechaActual.getMonth() + 1);
     renderCalendar(fechaActual);
@@ -96,14 +81,12 @@ function mesSiguiente() {
 btnPrev.addEventListener("click", mesAnterior);
 btnNext.addEventListener("click", mesSiguiente);
 
-//POPUP
 const popup = document.querySelector(".js-popu");
 const popupTitulo = document.getElementById("popup-titulo");
 const popupDescripcion = document.getElementById("popup-descripcion");
 const popupLink = document.getElementById("popup-link");
 const popupClose = document.querySelector(".js-popup-close");
 
-//muestra popup
 function mostrarPopup(titulo, descripcion, link) {
     popupTitulo.textContent = titulo;
     popupDescripcion.innerHTML = descripcion;
@@ -112,14 +95,12 @@ function mostrarPopup(titulo, descripcion, link) {
     popup.classList.remove("popup--hidden");
 }
 
-//oculta popup
 function ocultarPopup() {
     popup.classList.add("popup--hidden");
 }
 
 popupClose.addEventListener("click", ocultarPopup);
 
-//agrega los eventos al clic
 function agregarListenersEventos() {
     const eventosHTML = document.querySelectorAll(".event");
     for (let i = 0; i < eventosHTML.length; i++) {
