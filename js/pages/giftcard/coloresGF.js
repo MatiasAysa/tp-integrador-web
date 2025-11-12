c05e5bad98a5374ea25b1cddfe80c4356554f537
Ivan <ivansgamboa1979@gmail.com>
2025-11-12 19:35:42 -0300

    Calendario y homepage añadidas y adaptadas

---
+++ js/pages/giftcard/coloresGF.js
@@ -0,0 +1,41 @@
+import { Colores } from '../components/colors.js';
+
+export class ColoresGF {
+
+    constructor() { }
+
+    render(nombreView) {
+
+        const COLORES = new Colores();
+
+        // const nombreView = document.querySelector("#background__p--nombre-js");
+
+        const coloresForm = document.querySelectorAll(".radioWrap__labelRadio-js");
+        const coloresInput = document.querySelectorAll(".color__input");        
+
+        coloresForm.forEach( (color, i) => {
+            color.addEventListener('click', (e) => {
+
+                switch (coloresInput[i].value) {
+                    case 'azul':                        
+                        nombreView.style.color = COLORES.COLOR_PRINCIPAL;
+                        break; 
+                    case 'azul2':
+                        nombreView.style.color = COLORES.COLOR_PRINCIPAL_DEGRADADO;
+                        break;
+                    case 'amarillo':
+                        nombreView.style.color = COLORES.COLOR_SECUNDARIO;
+                        break;
+                    case 'amarillo2':
+                        nombreView.style.color = COLORES.COLOR_SECUNDARIO_DEGRADADO; 
+                        break; 
+                    case 'blanco':
+                        nombreView.style.color = COLORES.COLOR_TEXTO;
+                        break;
+                }
+
+            });
+        });
+
+    }
+}
