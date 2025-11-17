555f749fae00309af7a1badba0b6d23b4e8eca36
Ivan <ivansgamboa1979@gmail.com>
2025-11-17 18:04:20 -0300

    Limpieza de archivos

--- js/utils/carritoSlibear.js
+++
@@ -1,2 +0,0 @@
-[InternetShortcut]
-URL=file:///c%3A/Users/MATIAS/Desktop/UNLAM-TecnicaturaWeb/PRIMER%20A%C3%91O/ProgramacionWeb/Tp%20Integrador%20Web/tp-integrador-web/tp-integrador-web/js/utils/carritoSlibear.js
